f24fe5adacecdaceb4e0f04f91fbbee1c29b1d13
T <T>
2017-03-03 19:43:33 -0300

    se agrega screen add events

--- php/screen_add_event.php
+++ php/screen_add_event.php
@@ -5,13 +5,14 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="shortcut icon" href="../img/logo/favicon.ico">
-    <title> Ante Meridiem </title>
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Six+Caps" rel="stylesheet">
+
+	<style>
+	</style>
 
   </head>
 
-  <body class="footerPage">
+  <body class="screen_addEvent">
 
 
     <div class="contenido" >
@@ -36,7 +37,7 @@
         </div>
         <div class="header-login">
 
-          <img src="img/images/icon-user.png" alt="User">
+          <img src="../img/images/icon-user.png" alt="User">
           <form method="POST" action="php/login.php" >
             <label class="user">
               <input type="text" placeholder="User" name="user_name" required>
@@ -54,7 +55,7 @@
             Acceder
           </button>
 
-              <a href="http://localhost/WebMaster/php/screen_register.php">  Registrarse </a>
+              <a href="http://localhost:8888/Sitio%202.0/registro.php">  Registrarse </a>
 
           </form>
 
@@ -76,10 +77,94 @@
 
       <!-- ================== MAIN-CONTENT =============-->
 
-      <h2 class="back"> <a href="http://localhost/WebMaster/index.php#home"> < Volver al Home </a> </h2>
+      <h2 class="back"> <a href="http://localhost:8888/Sitio%202.0#home"> < Volver al Home </a> </h2>
+
+		<div class="home-text">
+          <h2 class="home-text">Lorem ipsum dolor sit amet</h2>
+          <p class="home-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis quam in massa fringilla pulvinar. Ut eget velit et neque feugiat tempor sit amet vitae enim. Aenean mattis felis non eros egestas, at aliquam ligula bibendum. Pellentesque viverra, felis nec lacinia rhoncus, nisi orci pulvinar ante, non accumsan turpis nisl sed sapien </p>
+        </div>
+
+		<div class="addEvent-mainContent">
+
+			<div class="addEvent">
+
+				<h2> Crear Evento </h2>
+
+				<div class="addEvent-left">
+					<form action="" method="" id="crearEvento">
+
+						<label>
+							Nombre del Evento:
+							<br>
+							<input type="text" >
+						</label>
+						<br>
+						<label>
+							Direccion:
+							<br>
+							<input type="text" >
+						</label>
+						<br>
+						<label>
+						Inicia:
+							<br>
+							<input class="date" type="datetime-local" >
+						</label>
+						<br>
+						<label>
+							Finaliza:
+							<br>
+							<input class="date" type="datetime-local" >
+						</label>
+						<br>
+						<label>
+							Genero:
+
+							<select name="genero">
+								<option value="rock">Rock</option>
+								<option value="funk">Funk</option>
+								<option value="soul">Soul</option>
+								<option value="reggae">Reggae</option>
+							</select>
+
+
+						</label>
+						<br>
+						<label class="upImage">
+							Imagen:
+
+							<input type="file" >
+						</label>
+
+					</form>
+				</div>
+				<div class="addEvent-right">
 
 
 
+						<label>
+							Informacion:
+							<br>
+							<textarea form="crearEvento" rows="3" cols="50" placeholder="Describe yourself here..."></textarea>
+						</label>
+						<br>
+						<label>
+							Descripcion:
+							<br>
+							<textarea form="crearEvento" rows="6" cols="50" placeholder="Describe yourself here..."></textarea>
+						</label>
+
+					<br>
+
+
+				</div>
+				<input id="enviarEvento" type="submit" value="Crear Evento">
+			</div>
+
+		</div>
+
+			<div class="black">
+			</div>
     <!-- ================== FOOTER ================-->
 
     <div class="footer">
